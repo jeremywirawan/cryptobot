180b1a982867fb2474d83997b124d70c438b942d
jeremywirawan <jeremywirawan.jw@gmail.com>
2026-05-24 14:04:00 +0700

    Update paper_wallet.py

--- paper_wallet.py
+++ paper_wallet.py
@@ -1,19 +1,42 @@
-cash = 1000000
+import json
+import os
 
-holdings = {
-    "BTC/IDR": 0,
-    "ETH/IDR": 0,
-    "SOL/IDR": 0
+WALLET_FILE = "wallet.json"
+
+default_wallet = {
+    "cash": 1000000,
+
+    "holdings": {
+        "BTC/IDR": 0,
+        "ETH/IDR": 0,
+        "SOL/IDR": 0
+    },
+
+    "last_buy_price": {
+        "BTC/IDR": 0,
+        "ETH/IDR": 0,
+        "SOL/IDR": 0
+    },
+
+    "last_trade_time": None,
+
+    "total_trades": 0,
+    "winning_trades": 0,
+    "losing_trades": 0
 }
 
-last_buy_price = {
-    "BTC/IDR": 0,
-    "ETH/IDR": 0,
-    "SOL/IDR": 0
-}
+def load_wallet():
 
-last_trade_time = None
+    if not os.path.exists(WALLET_FILE):
 
-total_trades = 0
-winning_trades = 0
-losing_trades = 0
+        save_wallet(default_wallet)
+
+    with open(WALLET_FILE, "r") as file:
+
+        return json.load(file)
+
+def save_wallet(data):
+
+    with open(WALLET_FILE, "w") as file:
+
+        json.dump(data, file, indent=4)
