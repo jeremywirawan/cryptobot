d629aa9405e9d5dca9cabffc10880618f1a80077
Jeremy Wirawan <jeremywirawan.jw@gmail.com>
2026-05-28 21:35:30 +0700

    balanced strategy update

--- strategy.py
+++ strategy.py
@@ -1,6 +1,8 @@
 import pandas as pd
+
 from ta.momentum import RSIIndicator
 from ta.trend import EMAIndicator
+
 
 def check_signal(ohlcv):
 
@@ -17,37 +19,53 @@ def check_signal(ohlcv):
     )
 
     # RSI
-    rsi_indicator = RSIIndicator(df['close'], window=14)
+    rsi_indicator = RSIIndicator(
+        df['close'],
+        window=14
+    )
+
     df['rsi'] = rsi_indicator.rsi()
 
+    # EMA50
+    ema50_indicator = EMAIndicator(
+        close=df['close'],
+        window=50
+    )
+
+    df['ema50'] = ema50_indicator.ema_indicator()
+
     # EMA200
-    ema_indicator = EMAIndicator(
+    ema200_indicator = EMAIndicator(
         close=df['close'],
         window=200
     )
 
-    df['ema200'] = ema_indicator.ema_indicator()
+    df['ema200'] = ema200_indicator.ema_indicator()
 
     current_rsi = df['rsi'].iloc[-1]
 
+    current_ema50 = df['ema50'].iloc[-1]
     current_ema200 = df['ema200'].iloc[-1]
 
-    current_price = df['close'].iloc[-1]
-
     print(f"Current RSI: {current_rsi:.2f}")
-    print(f"Current EMA200: {current_ema200:.2f}")
+    print(f"EMA50: {current_ema50:.2f}")
+    print(f"EMA200: {current_ema200:.2f}")
 
-    # SIGNAL LOGIC
+    # BUY
     if (
-        current_rsi < 40
-        and current_price > current_ema200
+        current_ema50 > current_ema200
+        and current_rsi < 45
     ):
+
         signal = "BUY"
 
-    elif current_rsi > 65:
+    # SELL
+    elif current_rsi > 70:
+
         signal = "SELL"
 
     else:
+
         signal = "HOLD"
 
     return signal, current_rsi, current_ema200
